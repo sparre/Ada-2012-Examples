c2760afb8c058e97432ae03a35adabec2fb2d52b
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-05-01 09:06:17 +0200

    Have experimented with 'or else raise' in contract aspects, but it doesn't quite work yet.

--- src/array_subtypes.ads
+++ src/array_subtypes.ads
@@ -1,6 +1,9 @@
 package Array_Subtypes is
    subtype Digit_Character is Character range '0' .. '9';
    subtype Digit_String is String
-     with Dynamic_Predicate => (for all C of Digit_String
-                                  => C in Digit_Character);
+     with Dynamic_Predicate
+            => (for all C of Digit_String => C in Digit_Character
+                  --  or else raise Constraint_Error
+                  --  Bug box in GNAT GPL 2014 and GNAT-4.9
+               );
 end Array_Subtypes;
